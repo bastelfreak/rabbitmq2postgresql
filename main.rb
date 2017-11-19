05520bcdc0f4a8a98a77d2bad6219a074bd2bce6
Tim Meusel <tim@bastelfreak.de>
2017-11-19 21:24:55 +0100

    add jsonb support

--- main.rb
+++ main.rb
@@ -3,6 +3,10 @@
 require 'bunny'
 require 'sequel'
 
+# the Sequel magic is based on"
+# https://gist.github.com/janko-m/87865c47500a90302152
+# http://sequel.jeremyevans.net/rdoc/classes/Sequel/Database.html
+
 # connection settings for our rabbitmq and for postgres are stored in a
 # seperate file
 require './credentials.rb'
@@ -26,22 +30,38 @@ queue.bind('marcelliitest')
 
 ## database stuff
 DB = Sequel.connect(connection_details_postgres, logger: Logger.new('log/db.log'))
-# DB.create_table :payloads do
-#  primary_key :id
-#  String :metadata_info
-#  String :delivery_info
-#  String :payload
-# end
+DB.create_table? :payloads do
+  primary_key :id
+  String :metadata_info
+  String :delivery_info
+  String :payload
+end
+
+# New hot shit, jsonbbbbbbbbbbbbbbbbbbbbbbbbbbbb. Lets beeeeeeeeeeeeeeeeeeeeeeeeeee (A Jan Philipp Zymnt quote)
+DB.create_table? :payloadjsonbs do
+  primary_key :id
+  String :metadata_info
+  String :delivery_info
+  Jsonb :payload
+end
+
+# Reading and writing
+DB.extension :pg_array
+DB.extension :pg_json
+# Querying
+Sequel.extension :pg_array_ops
+Sequel.extension :pg_json_ops
 
 require './payload.rb'
+require './payloadjsonb.rb'
 # create a continious bind as a consumer to our queue
 queue.subscribe do |delivery_info, metadata, payload|
-  # puts "delivery_info: #{delivery_info}"
-  # puts "metadata: #{metadata}"
-  puts "payload: #{payload}"
-  Payload.new(delivery_info: delivery_info, metadata_info: metadata, payload: payload).save
+  #puts "delivery_info: #{delivery_info}"
+  #puts "metadata: #{metadata}"
+  #puts "payload: #{payload}"
+  Payloadjsonb.new(delivery_info: delivery_info, metadata_info: metadata, payload: payload).save
 end
 loop do
-  puts "message count: #{queue.message_count}"
+  #puts "message count: #{queue.message_count}"
   sleep 10
 end
